9f929073da63ab3e3937e8586da0782cdac2453d
janyinsah <ja.bondzie@aol.com>
2023-03-15 16:28:32 +0000

    Renamed python file, added frequencing smoothing, edited comments based on new addtions.

--- pitheremin.py
+++ pitheremin.py
@@ -18,6 +18,11 @@ print(pygame.mixer.get_init())
 
 MAIN_CHANNEL = pygame.mixer.Channel(0)
 
+# n is defined as the moving average filter, this was an attempt to try and smove out the frequency as it produced arpeggiated sounds.
+n = 10
+# This numpy function takes the last average of n and assigns it to a variable called window.
+window = np.ones(n)/n # Define a Hann window for signal procesing.
+
 # Initialize mediapipe hands.
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
@@ -28,11 +33,17 @@ def genSineWave(Freq, Amp):
     Freq = np.reshape(Freq, (-1, 1))
     SAMPLES = np.zeros((44100, 2), dtype=np.int16) # Initialize samples using a numpy array passing two channels as a tuple.
     SINE = np.sin(2 * np.pi * np.arange(44100) * Freq[0]/44100)
-    SCALED_WAVE = (SINE * 32767 * Amp[1]).astype(np.int16)
+    SCALED_WAVE = (SINE * 32767 * Amp[1]/2).astype(np.int16)
     SAMPLES[:, 0] = SCALED_WAVE  # Index the samples array by 2 so that it matches the format of the steromixer.
     SAMPLES[:, 1] = SCALED_WAVE  # It duplicates the number of samples across the stereo channel 
-    SOUND = pygame.sndarray.make_sound(SAMPLES) # Convert then generate the sine wave based of it's calculation of the position of the left/right hand co-ordinates.
+    SOUND = pygame.sndarray.make_sound(capGain(SAMPLES)) # Convert then generate the sine wave based of it's calculation of the position of the left/right hand co-ordinates.
     MAIN_CHANNEL.play(SOUND, loops = -1) # Plays the through the pygame channel.
+
+def capGain(samples): # Function to appy to samples tor reduce clipping sound.
+    maxAmp = np.iinfo(samples.dtype).max
+    gain = min(1, (maxAmp / 2) / np.max(np.abs(samples))) 
+    return (samples * gain).astype(samples.dtype)
+
 
 frame = cv2.VideoCapture(0) # Open up a frame, for webcam streaming.
 
@@ -76,10 +87,13 @@ while True: # Loop used to run infinitely.
             rightHandAmp = np.interp(rightHandCoord, [minCoord[1], maxCoord[1]], [0, 1])[:, np.newaxis] # np.newaxis adds an empty dimension so that numpy can interpolate rightHandAmps values to map it to the required range for amplitude of the wave.
 
 
-            # Generate a waveform using numpy, then send it to defined pygame channel above, this is so that the sound is constant throughout hand movement,
-            genSineWave(leftHandFreq, rightHandAmp.flatten()) # We flatten the rightHandAmp parameter as the original variable takes two dimensions and must be converted to one.
+            leftHandFreqSmoothed = np.convolve(leftHandFreq.flatten(), window, mode='same') # We flatten the left hand frequency to a 1D array so that it can be sequenced and convolved
+            # Hann window used for signal processing and the same means that the input (leftHandFreq) signal will still match the output signal. This is passed as a parameter to genSineWave instead of the original left hand frequency.
 
-            print(f"Left Hand Frequency: {leftHandFreq} Right Hand Frequency: {rightHandAmp}")
+            # Generate a waveform using numpy, then send it to defined pygame channel above, this is so that the sound is constant throughout hand movement,
+            genSineWave(leftHandFreqSmoothed, rightHandAmp.flatten()) # We flatten the rightHandAmp parameter as the original variable takes two dimensions and must be converted to one.
+            
+            print(f"Left Hand Frequency: {leftHandFreq} Right Hand Frequency: {rightHandAmp}") #
         else:
             MAIN_CHANNEL.stop() # If no hands are detected, then pygames mixer will not play any sound, as there are no co-ordinates present to generate a sine wave.
         frame = cv2.flip(frame, 1) # Makes the frame horizintal, used to properly detect left and right hands.
